fb5dbe477c234b42897d0ac8bd8a16f95235dcc4
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-26 15:38:17 -0300

    test: ensure Request.areGlobalsDefined returns false if SERVER is empty

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -12,6 +12,8 @@ class RequestTest extends TestCase
     public function setUp(): void
     {
         $_POST = ['any_key' => 'any_value'];
+        $_GET = ['any_key' => 'any_value'];
+        $_SERVER = ['any_key' => 'any_value'];
         $this->sut = new Request();
     }
 
@@ -24,6 +26,12 @@ class RequestTest extends TestCase
     public function test_assert_areGlobalsDefined_returns_false_if_GET_is_not_defined(): void
     {
         unset($_GET);
+        self::assertFalse($this->sut->areGlobalsDefined());
+    }
+
+    public function test_assert_areGlobalsDefined_returns_false_if_SERVER_is_empty(): void
+    {
+        $_SERVER = [];
         self::assertFalse($this->sut->areGlobalsDefined());
     }
 }
